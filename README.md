38a6345666c2af018e56eeef2cb65a2c8395769b
HonestWorker <38898493+honestworker@users.noreply.github.com>
2019-01-25 12:00:53 +0800

    V 1.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# dailylifematters
-Driver Insurance Sites with Bootstrap Template
+# Driver Insurance Sites with Bootstrap Template
+
+## Live Site
+
+http://dailylifematters.com
+
+## Change Log
+
+1.1 2019/01/25
+    - Build the pages
+        autoagentsecrets.html
+        autoinsnewrule.html
+        homeprograms.html
+        homeprograms.html
+        privacy-policy.html
+        terms-of-use.html
+        homecash77k/wtsm.html
+
+1.0 2019/01/24
+    Init Version
---
+++ js/script.js
@@ -0,0 +1,67 @@
+var state_dict = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "District Of Columbia": "DC",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+};
+
+$.getJSON('https://geoip-db.com/json/').done (function(location) {
+    if (location.country_name == 'United States') {
+        $('body').removeClass('hidden');
+        var country_name = location.country_name;
+        var state_name = location.state;
+        var state_code = state_dict[state_name];
+        if (typeof state_code !== "undefined") {
+            $('.state-intro').html(state_name.toUpperCase() + ' Drivers — New policies show that for years many drivers have likely overpaid on their car insurance.');
+            $('.state-rule').html('"New Rule in ' + state_name + '".');
+            $('img.content-img').attr('src', './img/states/' + state_code + '.jpg');
+        }
+    }
+});
